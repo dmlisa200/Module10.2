87ceb6f87549b3a021f5f21cf39d0226d7bc600e
dmlisa200 <17917202+dmlisa200@users.noreply.github.com>
2020-07-01 23:18:39 -0500

    Add files via upload

---
+++ test_Student_class.py
@@ -0,0 +1,28 @@
+import unittest
+from Student_class import Student
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.student = student.Student('Kilmer, 'Lisa')
+
+    def tearDown(self):
+        del self.student
+
+    def test_object_created_required_attributes(self):
+        self.assertEqual(self.student.last_name, 'Kilmer')
+        self.assertEqual(self.student.first_name, 'Lisa')
+
+    def test_object_created_all_attributes(self):
+        student = student.Student('Kilmer', 'Lisa', 'CS', '3.5')
+        assert student.last_name == 'Kilmer'
+        assert student.first_name == 'Lisa'
+        assert student.major == 'CS'
+        assert student.gpa == '3.5'
+
+    def test_student_str(self):
+        self.assertEqual(self.student.first_name, 'Lisa')
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
